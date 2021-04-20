ac26f6a280ea2a7de85629095779c58e9997e8ce
csicsi2000 <69510072+csicsi2000@users.noreply.github.com>
2021-04-20 13:39:56 +0200

    optimization
    Some big images are coverted to smaller files. Using google hosted jQuery. Fixes for smaller displays. Added dissapearing navbar.

--- Js/common.js
+++ Js/common.js
@@ -11,12 +11,21 @@ function showPage() {
   document.body.classList.remove('js-loading');
 }
 
-/*
-$(window).load(function() {
-    $("body").removeClass("preload");
-})
+const body = document.body;
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+    var currentScrollPos = window.pageYOffset;
+    var isChecked = $("#toggle").is(":checked");
 
-window.onload = function() {
-    document.body.classList.remove('preload');
+    if (window.matchMedia("(orientation: landscape)").matches){
+        document.getElementById("toggle").checked = false;
+        isChecked == false;
+    }
+
+    if (prevScrollpos > currentScrollPos || isChecked == true) {
+        document.getElementById("navbar").style.top = "0";
+    } else {
+        document.getElementById("navbar").style.top = "-60px";
+    }
+  prevScrollpos = currentScrollPos;
 }
-*/
